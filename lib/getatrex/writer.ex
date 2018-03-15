268f4e442316a41999f996a074f443a3a4046c4e
Alex Filatov <alex@alexfilatov.com>
2018-03-15 08:51:07 +0000

    added moduledoc to writer

--- lib/getatrex/writer.ex
+++ lib/getatrex/writer.ex
@@ -1,4 +1,13 @@
 defmodule Getatrex.Writer do
+  @moduledoc """
+  Module responsible for writing translations to a file
+  Usage:
+    1. Start the generic server by Getatrex.Writer.start_link(filename)
+      where the filename if the name of the file you are going to write data
+    2. Send messages to server with payload you need to write to a file:
+      Getatrex.Writer.write(payload)
+      where payload could be simple string or %Getatrex.Message{} struct
+  """
   use GenServer
 
   def start_link(filename) do
@@ -10,7 +19,7 @@ defmodule Getatrex.Writer do
   end
 
   def write(%Getatrex.Message{} = message) do
-    
+
   end
 
   def write(line) do
