fb083d7b096c29d977a75674d92ca57feff7f12d
Alex Filatov <alex@alexfilatov.com>
2018-04-09 09:45:11 +0100

    removed not needed setup

--- test/getatrex/writer_test.exs
+++ test/getatrex/writer_test.exs
@@ -4,14 +4,13 @@ defmodule Getatrex.WriterTest do
   import Getatrex.Test.Helper
 
   setup do
-    filename = "filename.txt"
     {:ok, pid} = reset_file() |> Writer.start_link()
 
-    %{pid: pid, filename: filename}
+    %{pid: pid}
   end
 
   describe "writing single lines" do
-    test "writing single line", %{filename: _filename} do
+    test "writing single line" do
       line = "hello world\n"
 
       Writer.write(line)
@@ -21,7 +20,7 @@ defmodule Getatrex.WriterTest do
   end
 
   describe "writing Message structs" do
-    test "with one mention", %{filename: _filename} do
+    test "with one mention" do
       message = %Getatrex.Message{
         mentions: ["#: web/templates/layout/top_navigation.html.eex:17"],
         msgid: "Home",
@@ -33,7 +32,7 @@ defmodule Getatrex.WriterTest do
       assert file_contents() == ["", "#: web/templates/layout/top_navigation.html.eex:17", ~s(msgid "Home"), ~s(msgstr "Haus")] |> Enum.join("\n")
     end
 
-    test "with multiple mentions", %{filename: _filename} do
+    test "with multiple mentions" do
       message = %Getatrex.Message{
         mentions: ["#: web/templates/layout/top_navigation.html.eex:17", "#: web/templates/layout/top_navigation.html.eex:18", "#: web/templates/layout/top_navigation.html.eex:19"],
         msgid: "Home",
