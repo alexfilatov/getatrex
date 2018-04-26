f6147c6399f0d8897c28e99217e049b4ec8e4684
Alex Filatov <alex@alexfilatov.com>
2018-04-26 18:34:55 +0100

    changed the goth config again

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,7 @@
 use Mix.Config
 
 # GOOGLE AUTH
-config :goth, json: {:system, "GOOGLE_APPLICATION_CREDENTIALS"}
+config :goth,
+  json: "goth_credentials.json" |> File.read!
 
 import_config "#{Mix.env}.exs"
